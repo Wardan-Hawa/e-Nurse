a396a106042f6814aecc4d485754f7c8913136a8
Wardan Hawa <117356439+Wardan-Hawa@users.noreply.github.com>
2025-11-24 17:21:11 +0100

    Update index.js

--- e-nurse_API/routes/index.js
+++ e-nurse_API/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.use("/users", require("../lib/users/routers"));
 router.use("/interactions", require("../lib/interaction/routers"));
 router.use("/report", require("../lib/report/routers"));
