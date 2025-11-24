41418d6ece9dab344501ef113b0df5fde29e6a7d
Wardan Hawa <117356439+Wardan-Hawa@users.noreply.github.com>
2025-11-24 17:17:50 +0100

    Update consulting.js

--- e-nurse_API/models/consulting.js
+++ e-nurse_API/models/consulting.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Consulting extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  
     static associate(models) {
       // define association here
       Consulting.hasMany(models.Message, {foreignKey: "consultingId"})
