3281d96dae646a72790877fbc01e14396537f44f
Wardan Hawa <117356439+Wardan-Hawa@users.noreply.github.com>
2025-11-24 17:21:39 +0100

    Update 20230214151406-Specialities.js

--- e-nurse_API/seeders/20230214151406-Specialities.js
+++ e-nurse_API/seeders/20230214151406-Specialities.js
@@ -3,15 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  
     await queryInterface.bulkInsert("Specialities", [
       {
         name: "Allergy and Immunology",
@@ -77,12 +69,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+   
     await queryInterface.bulkDelete('Specialities', null, {});
   }
 };
