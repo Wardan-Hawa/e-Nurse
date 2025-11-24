d5c5d9ce141afa72892718aa963ed2f528d78e02
Wardan Hawa <117356439+Wardan-Hawa@users.noreply.github.com>
2025-11-24 17:18:07 +0100

    Update consulting.js

--- e-nurse_API/models/consulting.js
+++ e-nurse_API/models/consulting.js
@@ -6,7 +6,6 @@ module.exports = (sequelize, DataTypes) => {
   class Consulting extends Model {
   
     static associate(models) {
-      // define association here
       Consulting.hasMany(models.Message, {foreignKey: "consultingId"})
       Consulting.belongsTo(models.User, {foreignKey: "userId"})
       Consulting.belongsTo(models.Report, {foreignKey: "reportId"})
